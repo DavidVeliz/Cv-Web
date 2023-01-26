f86b22d39a3a3ff1efa64f81bb074eaf6bbeff46
DavidVeliz <122913443+DavidVeliz@users.noreply.github.com>
2023-01-26 12:54:42 -0300

    Se carga proyecto

---
+++ js/Funcion.js
@@ -0,0 +1,26 @@
+document.getElementById('claro').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'rgb(174, 185, 185)';
+    document.body.style.color = 'black';
+    document.getElementById('Uni').style.color = 'black';
+    document.getElementById('exp').style.color = 'black';
+    document.getElementById('info').style.color = 'black';
+    document.getElementById('egg').style.color = 'black';
+    document.getElementById('ap').style.color = 'black';
+    document.getElementById('curso').style.color = 'white';
+    document.getElementById('curso1').style.color = 'white';
+    document.getElementById('Uni1').style.color = 'white';
+
+})
+
+document.getElementById('oscuro').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'rgb(1, 1, 7';
+    document.body.style.color = 'aliceblue';
+    document.getElementById('Uni').style.color = 'white';
+    document.getElementById('exp').style.color = 'cyan';
+    document.getElementById('info').style.color = 'cyan';
+    document.getElementById('egg').style.color = 'white';
+    document.getElementById('ap').style.color = 'white';
+    document.getElementById('curso').style.color = 'cyan';
+    document.getElementById('curso1').style.color = 'cyan';
+
+})
